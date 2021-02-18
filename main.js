57d3265624a252aca52cbf755e00f2673fdcc4bc
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-02-18 12:47:02 -0800

    add event listeners on window for up, down, left and right. #2

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 var $raceCar = document.querySelector('.race-car');
 
 function changeDirection(event) {
+  if (event.key === 'ArrowUp') {
+    $raceCar.className = 'race-car up';
+  } else if (event.key === 'ArrowDown') {
+    $raceCar.className = 'race-car down';
+  } else if (event.key === 'ArrowLeft') {
+    $raceCar.className = 'race-car left';
+  } else if (event.key === 'ArrowRight') {
+    $raceCar.className = 'race-car right';
+  }
 
 }
-$raceCar.addEventListener('keydown', changeDirection);
+window.addEventListener('keydown', changeDirection);
