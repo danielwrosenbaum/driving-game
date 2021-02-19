7b58c0d4919543a9bd2dc85c098577dfa9214ce6
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-02-18 16:47:05 -0800

    Cleaned up code. #3

--- main.js
+++ main.js
@@ -20,28 +20,28 @@ function changeDirection(event) {
 function goButton(event) {
   // if (event.keyCode === 32) {
   if ($raceCar.className === 'race-car right') {
-    carLeft += 5;
-    $raceCar.style.left = carLeft + 'px';
-    if (carLeft >= 900) {
-      carLeft -= 5;
-    }
-  } else if ($raceCar.className === 'race-car left') {
-    carLeft -= 5;
-    $raceCar.style.left = carLeft + 'px';
-    if (carLeft <= 0) {
+    if (carLeft < 900) {
       carLeft += 5;
+      $raceCar.style.left = carLeft + 'px';
+    }
+
+  } else if ($raceCar.className === 'race-car left') {
+    if (carLeft >= 0) {
+      carLeft -= 5;
+      $raceCar.style.left = carLeft + 'px';
+
     }
   } else if ($raceCar.className === 'race-car down') {
-    carTop += 5;
-    $raceCar.style.top = carTop + 'px';
-    if (carTop >= 640) {
-      carTop -= 5;
+    if (carTop < 640) {
+      carTop += 5;
+      $raceCar.style.top = carTop + 'px';
+
+      // carTop -= 5;
     }
   } else if ($raceCar.className === 'race-car up') {
-    carTop -= 5;
-    $raceCar.style.top = carTop + 'px';
-    if (carTop <= 0) {
-      carTop += 5;
+    if (carTop >= 0) {
+      carTop -= 5;
+      $raceCar.style.top = carTop + 'px';
     }
   }
   // }
