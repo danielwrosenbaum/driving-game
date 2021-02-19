59a64216fa137c66eb418dea883d8a87a652c694
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-02-18 16:06:25 -0800

    Added interval timer to page and made interval time respond to spacebar. #3

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var $raceCar = document.querySelector('.race-car');
 var carLeft = 0;
 var carTop = 0;
 function changeDirection(event) {
+
   if (event.key === 'ArrowUp') {
     $raceCar.className = 'race-car up';
   } else if (event.key === 'ArrowDown') {
@@ -11,37 +12,40 @@ function changeDirection(event) {
   } else if (event.key === 'ArrowRight') {
     $raceCar.className = 'race-car right';
   }
-}
-
-function goButton(event) {
   if (event.keyCode === 32) {
-    if ($raceCar.className === 'race-car right') {
-      carLeft += 5;
-      $raceCar.style.left = carLeft + 'px';
-      if (carLeft >= 900) {
-        carLeft -= 5;
-      }
-    } else if ($raceCar.className === 'race-car left') {
-      carLeft -= 5;
-      $raceCar.style.left = carLeft + 'px';
-      if (carLeft <= 0) {
-        carLeft += 5;
-      }
-    } else if ($raceCar.className === 'race-car down') {
-      carTop += 5;
-      $raceCar.style.top = carTop + 'px';
-      if (carTop >= 640) {
-        carTop -= 5;
-      }
-    } else if ($raceCar.className === 'race-car up') {
-      carTop -= 5;
-      $raceCar.style.top = carTop + 'px';
-      if (carTop <= 0) {
-        carTop += 5;
-      }
-    }
+    setInterval(goButton, 16);
   }
 }
 
+function goButton(event) {
+  // if (event.keyCode === 32) {
+  if ($raceCar.className === 'race-car right') {
+    carLeft += 5;
+    $raceCar.style.left = carLeft + 'px';
+    if (carLeft >= 900) {
+      carLeft -= 5;
+    }
+  } else if ($raceCar.className === 'race-car left') {
+    carLeft -= 5;
+    $raceCar.style.left = carLeft + 'px';
+    if (carLeft <= 0) {
+      carLeft += 5;
+    }
+  } else if ($raceCar.className === 'race-car down') {
+    carTop += 5;
+    $raceCar.style.top = carTop + 'px';
+    if (carTop >= 640) {
+      carTop -= 5;
+    }
+  } else if ($raceCar.className === 'race-car up') {
+    carTop -= 5;
+    $raceCar.style.top = carTop + 'px';
+    if (carTop <= 0) {
+      carTop += 5;
+    }
+  }
+  // }
+}
+// var raceTimer = setInterval(goButton, 16);
 window.addEventListener('keydown', changeDirection);
-window.addEventListener('keypress', goButton);
+window.addEventListener('keydown', goButton);
